f81b2f717bf83f4b346e34598170153b9eed5d67
BryanCox <bryanc71@yahoo.com>
2018-01-30 10:36:05 -0700

    static dynamics complete

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-// axios.get('http://localhost:3000/')
-// .then(response => JSON.stringify(response))
-// .then(arr => {
-//
-// console.log(arr)
-// })
-document.getElementById('main-blurb').innerHTML = "this is the main blurb"
+fetch('http://localhost:3000/db')
+.then(response => response.json())
+.then(arr => {
+  console.log(arr)
+  document.getElementById('main-blurb').addEventListener('click', learnMore)
+  document.getElementById('main-blurb').innerHTML = arr.dcr[0].title
+  document.getElementById('second-title-one').innerHTML = arr.Minutemen[8].title
+  document.getElementById('reporter1').innerHTML = 'Piper Wright'
+  document.getElementById('second-title-two').innerHTML = arr.Minutemen[2].title
+  document.getElementById('reporter2').innerHTML = 'Travis Miles'
+// modal news object
+function learnMore(e){
+console.log('click')
+  document.getElementById('headlineModalContent').innerHTML = arr.dcr[0].story
+  document.getElementById('headlineModalLabel').innerHTML = arr.dcr[0].title
+  }
+})
