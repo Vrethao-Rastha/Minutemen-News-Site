34f27da73ded10afc8890cf250b8de82660bcc11
BryanCox <bryanc71@yahoo.com>
2018-01-30 21:15:37 -0700

    pictures added

--- script.js
+++ script.js
@@ -1,68 +1,102 @@
 fetch('http://localhost:3000/db')
-.then(response => response.json())
-.then(arr => {
-  // console.log(arr)
+  .then(response => response.json())
+  .then(arr => {
+    // console.log(arr)
     // static headlines and pictures
-  document.getElementById('main-blurb').addEventListener('click', learnMore)
-  document.getElementById('main-blurb').innerHTML = arr.dcr[0].title
-  document.getElementById('second-title-one').innerHTML = arr.Minutemen[8].title
-  document.getElementById('reporter1').innerHTML = 'Piper Wright'
-  document.getElementById('second-title-two').innerHTML = arr.dcr[7].title
-  document.getElementById('reporter2').innerHTML = 'Travis Miles'
-    // navbar buttons
-  document.getElementById('blog').addEventListener('click', blogPage)
-  document.getElementById('piper').addEventListener('click', piperPage)
-  document.getElementById('report').addEventListener('click', reportPage)
+    document.getElementById('main-blurb').addEventListener('click', learnMore)
+    document.getElementById('main-blurb').innerHTML = arr.dcr[0].title
+    document.getElementById('second-title-one').innerHTML = arr.Minutemen[8].title
+    document.getElementById('reporter1').innerHTML = 'Piper Wright'
+    document.getElementById('second-title-two').innerHTML = arr.dcr[7].title
+    document.getElementById('reporter2').innerHTML = 'Travis Miles'
 
-  // modal checkbox function
+    // modal form validation function
 
-  document.getElementById('exampleCheck1').addEventListener('click', nonSynth)
-    function nonSynth(e){
-      
+    document.getElementById('exampleCheck1').addEventListener('click', nonSynth)
+
+    function nonSynth(e) {
       document.getElementById('submit-button').classList.remove('disabled');
     }
-  // loop for dynamic story generation
-  for (let i = 0; i < arr.Minutemen.length - 4; i++){
+    // loop for dynamic story generation
+    for (let i = 0; i < arr.Minutemen.length - 4; i++) {
 
-  document.getElementById(`minutemen-headline-${i+1}`).innerHTML = arr.Minutemen[i].title
-  document.getElementById(`dcr-headline-${i+1}`).innerHTML = arr.dcr[i+1].title
+      document.getElementById(`minutemen-headline-${i+1}`).innerHTML = arr.Minutemen[i].title
+      document.getElementById(`dcr-headline-${i+1}`).innerHTML = arr.dcr[i + 1].title
 
-  // document.getElementById('minutemen-headline-1').addEventListener('click', minutemenStory)
-  // document.getElementById('minutemen-headline-2').addEventListener('click', minutemenStory)
-  // document.getElementById('minutemen-headline-3').addEventListener('click', minutemenStory)
-  // document.getElementById('minutemen-headline-4').addEventListener('click', minutemenStory)
-  // document.getElementById('minutemen-headline-5').addEventListener('click', minutemenStory)
+      // minutemen side story block
+      document.getElementById('minutemen-headline-1').addEventListener('click', minutemenStory1)
+      document.getElementById('minutemen-headline-2').addEventListener('click', minutemenStory2)
+      document.getElementById('minutemen-headline-3').addEventListener('click', minutemenStory3)
+      document.getElementById('minutemen-headline-4').addEventListener('click', minutemenStory4)
+      document.getElementById('minutemen-headline-5').addEventListener('click', minutemenStory5)
 
-  // document.getElementById('dcr-headline-1').addEventListener('click', dcrStory)
-  // document.getElementById('dcr-headline-2').addEventListener('click', dcrStory)
-  // document.getElementById('dcr-headline-3').addEventListener('click', dcrStory)
-  // document.getElementById('dcr-headline-4').addEventListener('click', dcrStory)
-  // document.getElementById('dcr-headline-5').addEventListener('click', dcrStory)
+      function minutemenStory1() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.Minutemen[0].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.Minutemen[0].story
+      }
+
+      function minutemenStory2() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.Minutemen[1].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.Minutemen[1].story
+      }
+
+      function minutemenStory3() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.Minutemen[2].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.Minutemen[2].story
+      }
+
+      function minutemenStory4() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.Minutemen[3].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.Minutemen[3].story
+      }
+
+      function minutemenStory5() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.Minutemen[4].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.Minutemen[4].story
+      }
+
+      // diamond city radio side story block
+      document.getElementById('dcr-headline-1').addEventListener('click', dcrStory1)
+      document.getElementById('dcr-headline-2').addEventListener('click', dcrStory2)
+      document.getElementById('dcr-headline-3').addEventListener('click', dcrStory3)
+      document.getElementById('dcr-headline-4').addEventListener('click', dcrStory4)
+      document.getElementById('dcr-headline-5').addEventListener('click', dcrStory5)
 
 
-  // function minutemenStory(){
-  //   document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[0].story
-  //   document.getElementById('side-story-modal-content').innerHTML = arr.dcr[0].title
-  // }
+      function dcrStory1() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[1].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.dcr[1].story
+      }
+
+      function dcrStory2() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[2].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.dcr[2].story
+      }
+
+      function dcrStory3() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[3].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.dcr[3].story
+      }
+
+      function dcrStory4() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[4].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.dcr[4].story
+      }
+
+      function dcrStory5() {
+        document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[5].title
+        document.getElementById('side-story-modal-content').innerHTML = arr.dcr[5].story
+      }
 
 
 
-  }
+    }
 
-// modal news object function
-function learnMore(e){
-console.log('click')
-  document.getElementById('headlineModalContent').innerHTML = arr.dcr[0].story
-  document.getElementById('headlineModalLabel').innerHTML = arr.dcr[0].title
-  }
-})
-  // end of modal news object function
-function blogPage(){
-  console.log('click')
-}
-function piperPage(){
-  console.log('click')
-}
-function reportPage(){
-  console.log('click')
-}
+    // modal news object function
+    function learnMore(e) {
+      console.log('click')
+      document.getElementById('headlineModalContent').innerHTML = arr.dcr[0].story
+      document.getElementById('headlineModalLabel').innerHTML = arr.dcr[0].title
+    }
+  })
+// end of modal news object function
