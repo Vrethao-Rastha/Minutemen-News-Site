eec1dbba37021985ad6ab3d10fe25d6d433a62e3
BryanCox <bryanc71@yahoo.com>
2018-01-30 19:48:17 -0700

    form validation complete

--- script.js
+++ script.js
@@ -14,14 +14,31 @@ fetch('http://localhost:3000/db')
   document.getElementById('piper').addEventListener('click', piperPage)
   document.getElementById('report').addEventListener('click', reportPage)
 
-// loop for dynamic story generation
+  // modal checkbox function
+
+  document.getElementById('exampleCheck1').addEventListener('click', nonSynth)
+    function nonSynth(e){
+      
+      document.getElementById('submit-button').classList.remove('disabled');
+    }
+  // loop for dynamic story generation
   for (let i = 0; i < arr.Minutemen.length - 4; i++){
 
   document.getElementById(`minutemen-headline-${i+1}`).innerHTML = arr.Minutemen[i].title
   document.getElementById(`dcr-headline-${i+1}`).innerHTML = arr.dcr[i+1].title
 
-  // document.getElementById(`minutemen-headline-${i+1}`).addEventListener('click', minutemenStory)
-  // document.getElementById(`dcr-headline-${i+1}`).addEventListener('click', dcrStory)
+  // document.getElementById('minutemen-headline-1').addEventListener('click', minutemenStory)
+  // document.getElementById('minutemen-headline-2').addEventListener('click', minutemenStory)
+  // document.getElementById('minutemen-headline-3').addEventListener('click', minutemenStory)
+  // document.getElementById('minutemen-headline-4').addEventListener('click', minutemenStory)
+  // document.getElementById('minutemen-headline-5').addEventListener('click', minutemenStory)
+
+  // document.getElementById('dcr-headline-1').addEventListener('click', dcrStory)
+  // document.getElementById('dcr-headline-2').addEventListener('click', dcrStory)
+  // document.getElementById('dcr-headline-3').addEventListener('click', dcrStory)
+  // document.getElementById('dcr-headline-4').addEventListener('click', dcrStory)
+  // document.getElementById('dcr-headline-5').addEventListener('click', dcrStory)
+
 
   // function minutemenStory(){
   //   document.getElementById('side-story-modal-headline').innerHTML = arr.dcr[0].story
