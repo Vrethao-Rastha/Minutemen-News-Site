2fa317f71af1b9bd31fad6a09d06c614393ec09d
BryanCox <bryanc71@yahoo.com>
2018-02-01 00:35:24 -0700

    blog stories working

--- blog.js
+++ blog.js
@@ -8,25 +8,40 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
+fetch('http://localhost:3000/db')
+  .then(response => response.json())
+  .then(arr => {
+
+    // document.addEventListener("DOMContentLoaded", blogPopulator)
+
+
+
 document.getElementById('story-one').addEventListener('click', storyOne)
 document.getElementById('story-two').addEventListener('click', storyTwo)
 document.getElementById('story-three').addEventListener('click', storyThree)
 document.getElementById('story-four').addEventListener('click', storyFour)
 document.getElementById('story-five').addEventListener('click', storyFive)
 
+
 function storyOne(){
 openNav()
-document.getElementById('the-story').innerHTML = 'blah'
+document.getElementById('the-story').innerHTML = arr.blogs[0].blog
 }
 function storyTwo(){
 openNav()
+document.getElementById('the-story').innerHTML = arr.blogs[1].blog
 }
 function storyThree(){
 openNav()
+document.getElementById('the-story').innerHTML = arr.blogs[2].blog
 }
 function storyFour(){
 openNav()
+document.getElementById('the-story').innerHTML = arr.blogs[3].blog
 }
 function storyFive(){
 openNav()
+document.getElementById('the-story').innerHTML = arr.blogs[4].blog
 }
+
+})
