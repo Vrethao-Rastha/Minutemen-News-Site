92f0d62c5c3e8cebac3b68a7818be84d83495c28
BryanCox <bryanc71@yahoo.com>
2018-03-11 21:46:01 -0700

    asdf

--- blog.js
+++ blog.js
@@ -1,3 +1,29 @@
+let arr = {
+  "blogs": [
+  {
+    "id": "1",
+    "blog": "\r\nI’m from here. I was born in the Commonwealth, but I’ve been away for a long time.\r\nWhen I first set eyes on my home again, I thought I was lost. Or dead.\r\nI guess time has a way of smoothing over the corners of memory, but as I stood on the western border of the Commonwealth that day, I wasn’t sure it was worth going any further.\r\nThen some fool raider decided to try to shoot my dog.\r\nSo now this place can burn, as far as I’m concerned.\r\nI don’t know if she was bloodthirsty and tried to kill everyone she met out here, or was just hungry.\r\nIt the end, it hardly mattered. She tried to kill me, so I killed her.\r\nJust another day in the Commonwealth.\r\nThere is a distant part of me that often wonders: there are so few of us left. A shade, clinging to the bones of its former glory.\r\nI don’t even know how many I’ve killed. No more than tried to kill me to be sure, but every death brings humanity one step closer to our inevitable end.\r\nThere are those who say that we can yet rebuild, that extinction isn’t inevitable.\r\nPersonally, I think we died 200 years ago, and the body hasn’t stopped twitching yet.\r\nI think I’ll end this here. Even I can’t maintain this level of melancholy forever.\r\nJust another day in the Commonwealth…\r\n"
+  },
+  {
+    "id": "2",
+    "blog": "An old pre war saying came to mind today: As the world fell, each of us in our own way was broken. It was hard to know who was more crazy... me... or everyone else.\r\nI no longer remember the context, but I find it comforting.\r\nI went home today. Or to what used to be my home.\r\nAn abandoned ruin, just dry bones.\r\nIt feels like someone else’s life now. Did I really used to live here, or have I cracked and created this comforting illusion?\r\nA mother seeking a lost son. Not a new tale, for humanity in general, or in the Commonwealth specifically.\r\nIt would be easy enough to just lie down, stop wandering and end it. There is some appeal in that. Another old quote: Hope is a mistake. If you can’t fix whats broken, you’ll go insane.\r\nAnd yet here I am, writing this journal on this battered old Pipboy.\r\nI found some food and water in an old cellar behind one of these ruined houses.\r\nWhen the sun rises I’ll set off for Concord.\r\n"
+  },
+  {
+    "id": "3",
+    "blog": "I found my way to the old cemetery behind the drive-in today.\r\nNot sure what led my feet that direction. Maybe I had intended to find an open grave and fill it.\r\nNot sure there was any plan at all.\r\nI used to find the stone benches and trees comforting, I do remember that.\r\nI found none of that serenity this visit.\r\nWhat I did find was another lost soul.  A woman named Heather who had lost her sister.\r\nI think that collective loss brought us together, not that there’s anyone in this hollow place that doesn’t wear loss and pain as we used to wear designer clothing.\r\nIts nice to have someone around.\r\nIn all honesty, before she saw me and called out to me, I’m not sure the last time I spoke.\r\nThe difficulty I had in making my voice work was telling.\r\nAll those raiders I killed in the Air Force base, and the ones in the Corvega plant the following day, I don’t think I said a thing. I don’t even talk to the dog anymore.\r\nShe agreed to come back with me to the Red Rocket I’ve been living in.\r\nAs the light failed, distant gunfire echoed around the countryside.\r\nStrange what a person can get used to.\r\n"
+  },
+  {
+    "id": "4",
+    "blog": "I killed a man with a sledge hammer today.\r\nThat would normally hold more weight than it does these days.\r\nMy pipegun jammed, he was running at me with a tire iron in hand.\r\nI hit him in the chest, noting the odd vibrations that traveled up the handle as his ribs and sternum shattered.\r\nAs he lay there, choking on his own blood, I felt nothing. I left him there, drowning in blood.\r\nThat distant part of me that screams at night in my dreams was calm. I know this place is stealing the last shreds of my humanity.\r\nAll I want is a safe place for Heather and me, where we can live in peace and be left alone.\r\nEvery day I spend in this place moves me closer to those mindless feral ghouls that aimlessly wander the countryside.\r\nWhere must we wander this wasteland, we who search for our better selves?\r\n"
+  },
+  {
+    "id": "5",
+    "blog": "I met a man today. A broken idealist, much like I hope I once was.\r\nHe believes that the Commonwealth can be turned around. I’m not sure I agree.\r\nI don’t know why I agreed to help him.\r\nEven if we could turn everything around and create this perfect world, what place would there be for me in it?\r\nI’m a monster. A fiend. \r\nPerhaps the only road to redemption left to me is to die trying to create this brave new world.\r\nWere I a religious woman, I would be a lot more concerned about what may be waiting for me when this body finally fails.\r\nSo this is my new purpose in life. I now dedicate myself to trying to build something lasting on the bones of my failure.\r\nMaybe that will be enough.\r\n"
+  },
+]
+}
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "75%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -8,9 +34,9 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-fetch('http://localhost:3000/db')
-  .then(response => response.json())
-  .then(arr => {
+// fetch('http://localhost:3000/db')
+//   .then(response => response.json())
+//   .then(arr => {
 
 document.getElementById('story-one').addEventListener('click', storyOne)
 document.getElementById('story-two').addEventListener('click', storyTwo)
@@ -40,4 +66,4 @@ openNav()
 document.getElementById('the-story').innerHTML = arr.blogs[4].blog
 }
 
-})
+// })
--- script.js
+++ script.js
@@ -1,6 +1,153 @@
-fetch('http://localhost:3000/db')
-  .then(response => response.json())
-  .then(arr => {
+// fetch('http://localhost:3000/db')
+//   .then(response => response.json())
+//   .then(arr => {
+
+let arr = {
+  "dcr": [
+    {
+      "id": 1,
+      "title": "The fall of Ack-Ack and the Raiders of USAF Olivia:",
+      "story": "Ack-Ack. The Minigun weilding, pig-tail wearing leader of the Raiders of USAF Olivia. If you live in or have traveled through the Northwest part of The Commonwealth, you’ve likely heard of, or been the victim of her cruelty.\r\n        That all changed several days ago.\r\n        Trashcan Carla, a caravan owner that frequents that area, reported hearing gunfire coming from the old US Air Force facility.\r\n        Now normally that wouldn’t be much news, as Raiders tend to solve all their disputes with gunfire, but the sheer volume of fire caught her attention.\r\n        The following morning she told me she decided to have a look, and what she found was a rather uncomfortable.\r\n        I wont go into the details of what she found, but suffice to say, Ack-Ack and her war band wont be troubling anyone ever again.\r\n        Silly hairstyle aside, Ack-Ack was no pushover.\r\n        Be careful out there, Commonwealthers."
+    },
+    {
+      "id": 2,
+      "title": " Corvega Assembly plant goes silent:",
+      "story": "Its no secret that Jared and his warband own Lexington. Its also no secret that his forces had been moving west, and had moved into Concord as well.\r\n        Some here in Diamond City had feared he was on a path to gathering enough men and women that he would soon be an actual threat to The Great Green Jewel.\r\n        While many would see that as fatalism, and that nothing can breach our walls, the point is now moot.\r\n        A very brave scaver of my acquaintance tell me that she was attempting to sneak into Lexington under the cover of a Rad Storm. She had designs to look for salvage in the old Super Duper Mart.\r\n        While she was unsuccessful in that (too many Ghouls in the area) she tells me that as the storm passed, she saw a single, blood splattered figure emerge from the assembly plant with a equally macabre painted Mr Handy in tow.\r\n        She wisely stayed out of sight, and quickly left the city.\r\n        What happened inside, and who this mysterious figure could be remain a topic for speculation.\r\n        There can be no question, however, that Lexington has been very quiet of late.\r\n        Be careful out there, Commonwealthers."
+    },
+    {
+      "id": 3,
+      "title": "Something bad happened at Arcjet Systems:",
+      "story": "I know most of my readers never leave the safety of The Wall, but a few will know of the old Arcjet Systems building on the extreme west end of the Commonwealth.\r\n        While it had been abandoned for years, rumor has it that a scout party for The Brotherhood of Steel engaged with a Synth patrol in or around the facility.\r\n        Who was involved? What were they fighting over and why?\r\n        I know its unlikely we will get an answer to these questions, but if you were in the area, or have firsthand knowledge of what happened, please come forward to Diamond City Radio and tell your tale."
+    },
+    {
+      "id": 4,
+      "title": "Who is this mysterious Sole Survivor?",
+      "story": "Reports have been coming my way for several weeks now, a lone figure in a tattered Vault Suit with a Mr Handy and a dog in tow.\r\n        Normally that wouldn’t be much in the way of news, but for the fact that I’ve had several reports of a person of that description leaving the scenes of numerous… disputes.\r\n        If this is to be believed, we either have a new Raider Warlord on our hands, or something else less clear.\r\n        What we do know is that whomever this mysterious person be, they have been reported on or leaving the scenes of what can only be massacres of various raider war bands.\r\n        Who is this person? What do they want, and what do we make of this increasing pile of bodies?\r\n        Be careful out there, Commonwealthers."
+    },
+    {
+      "id": 5,
+      "title": "Massacre at University Point",
+      "story": "Well. I’m honestly at a loss as to how to proceed with this one…\r\n        University Point. I’m sure most of you know it, the trade settlement on the eastern edge of the city, right on the cusp of the bay.\r\n        I’m sure most of you also know by now, rumors have been swirling that it was attacked by the institute, and every man, woman and child was slaughtered.\r\n        At least in part, these rumors are true. Witnesses have come forward to inform me that The Institute was indeed responsible.\r\n        Reports put the count anywhere from 50 to 500 synth solders surrounded the settlement, and when it was over, there was no one left alive.\r\n        Keep your loved ones close, readers. There may not be a tomorrow."
+    },
+    {
+      "id": 6,
+      "title": "Identity of the Sole Survivor revealed!",
+      "story": "This story comes courtesy of my fellow journalist Piper Wright.\r\n        For weeks now, rumors have been swirling about a mysterious person, a Mr Handy and a dog, wandering the Commonwealth, and leaving a wake of bloody corpses and devastation behind them.\r\n        These rumors can’t all be true, as the deeds and places this person have been and done have swollen to impossible levels.\r\n        However, our very own Piper Wright of Publik Occurrences claims to have had an encounter with this mysterious person right outside the gates of Diamond City.\r\n        What we know: She goes by The Valkyrie. She claims not to eat little children, or drink the blood of the weak.\r\n        When asked about all the places she has reportedly been, and the body count the rumors credit her with, she simply replied, War Never Changes.\r\n        I know this has created more questions than it answers, but at least we have a name.\r\n        Perhaps we will learn more in the days to come."
+    },
+    {
+      "id": 7,
+      "title": "Return of Nick Valentine",
+      "story": "I’m sure all my readers know by now, that our Resident detective Nick Valentine went missing a few weeks ago.\r\n          Well, thanks to the Commonwealth’s newest resident, The Valkyrie, he has come home.\r\n          According to our own Ellie Perkins, The Valkyrie came into the office looking to hire Nick, only to find that he’d gone missing.\r\n          On Ellie’s behest, a deal was made and The Valkyrie agreed to find him and bring him back, being directed to the subway station under Boston Commons.\r\n          The details are sketchy, and Mr Valentine never talks about a case, but one of my informants reports Skinny Malone and his entire gang lay rotting in the corpse of Park Street Subway station.\r\n          This leads us again to the question: Who is this woman. What does she want? And how is one person leaving a trail of corpses in her wake that would give a Deathclaw a run for its money?\r\n          More as this story develops."
+    },
+    {
+      "id": 8,
+      "title": "The Mechanist",
+      "story": "As always in the Commonwealth, the rumor mill is in full tilt.\r\n        Reports have come to my attention about ragged bands of yellow robots wandering the ruins of the city, attacking anyone and everyone in their path, while playing holotapes claiming to be saving us from evil.\r\n        This is something of a contradiction, to be sure.\r\n        Until we have more to go on, all I can say is keep an eye out, and beware robots in yellow."
+    },
+    {
+      "id": 9,
+      "title": "Mystery of Boston Common solved",
+      "story": "Its no mystery to say that people who go to Boston Common don’t come back.\r\n        The rumor is that a swan kills them, but how can a 15 pound bird slay a grown man with a pipe rifle?\r\n        Well, an eye witness hiding in a ruined building claims to have seen The Valkyrie kill a truly massive Behemoth that called itself Swann.\r\n        The beast was wielding a telephone pole in a single hand and a bit of pipe attached to a fire hydrant in the other, but it was no match for this lone wanderer.\r\n        It lays dead on the shore of the radioactive pond in the center of the park, and a quick addendum to this report, the corpse is attracting quite a lot of Mirelurks, so I’d avoid it if all possible."
+    },
+    {
+      "id": 10,
+      "title": "New raider faction?",
+      "story": "In a report that closely resembles the one a few weeks ago about yellow robots attacking anything in their path, a rumor claims a new raider faction has been spotted in the western reaches of the Commonwealth.\r\n        They identify themselves as the Rust Devils, and would not be all that noteworthy from other raiders, but for the fact that they seem to have experts in robotics in their ranks, as every siting has included mention of gruesome Protectron or Assaultron robots.\r\n        Some have suggested, often around a bottle of Bobrov Vodka, that they are the solders of The Valkyrie. They go on to say that she is often seen in the company of that odd Mr Handy, and has a habit of leaving a large body count in her wake as well.\r\n        While both those facts are undeniably true, I don’t believe this is a fair claim.\r\n        For one thing, The Valkyrie has, as far as we know, only killed raiders, gunners and triggermen.\r\n        Our own Piper Wright and Nick Valentine claim, while obviously a little unhinged, they don’t believe she would kill innocent people. At least not without being provoked.\r\n        No, while the Commonwealth’s newest and most interesting resident may be many things, I don’t believe a raider warlord is one of them."
+    }
+  ],
+  "Minutemen": [
+    {
+      "id": 1,
+      "title": "Deathclaw spotted near The Slog",
+      "story": "Traders report spotting a Deathclaw near the road to The Slog.\r\n        All tarberry traders are warned to be on high alert as it is believed to still be in the area.\r\n        Although it was observed killing and eating a Bramin, as we all know, a Deathclaw will attack and kill without being hungry.\r\n        Again, all allied settlements and patrols in the area of The Slog should remain on high alert until further notice."
+    },
+    {
+      "id": 2,
+      "title": "Raider attack on Abernathy Farm",
+      "story": "A Minutemen patrol in the area reported a raider attack on Abernathy farm after speaking with Blake Abernathy. He stated that raiders based in USAF Olivia stole half his Tato crop and killed his daughter Mary.\r\n      A greater presence in the area is recommended, until enough troops can be mustered to drive them out of the Air Force ruins."
+    },
+    {
+      "id": 3,
+      "title": "Explosion near the Red Rocket south of Diamond City",
+      "story": "Reports claim it was a moonshiner that lost control of his stil, or perhaps was trying to cook chems at the same time.\r\n      No survivors, no further acton required."
+    },
+    {
+      "id": 4,
+      "title": "Super Mutant attack at Greentop Nursery",
+      "story": "Residents of Greentop Nursery reported a Supermutant raiding party attacked in the middle of the night, stealing several stewpots and old tires, as well as killing all their Rad-chickens and destroying their melon crops.\r\n      They are believed to still be in the area, possibly attached to the group that has been raiding out of Breakheart Banks.\r\n      High priority, assign a patrol to scout the area, and if possible kill or drive them out."
+    },
+    {
+      "id": 5,
+      "title": "Rogue Robot",
+      "story": "Citizens in the area of Nordhagen Beach reported seeing a pre war Mr Gutsy attempting to enforce a pre-war curfew order.\r\n      Where this thing came from or how its still operating is unclear, but it is known to have already killed a caravan group moving through the area.\r\n      High priority, get a heavy weapon team down there and scrap the fool thing before it finds somewhere more densely populated."
+    },
+    {
+      "id": 6,
+      "title": "Gunner attack at Jamaica Plain",
+      "story": "A small force of Gunners attacked Jamaica Plain last night.\r\n      The locals were able to repel the attack, but claim they feel it was merely a scouting action.\r\n      With the proximity of the Gunner fortress at Quincy, this is quite likely.\r\n      Recommend dispatching as many troops to the area as can be spared for the next week.\r\n      We don’t have the numbers to drive them out of Quincy, so maintaining a stalemate is our current best option."
+    },
+    {
+      "id": 7,
+      "title": "Raider attack at Oberland Station",
+      "story": "Residents of Oberland station report a raider attack two days ago.\r\n      This appears to be a small group, reportedly Clutch from Back Street Apparel.\r\n      Why Diamond City Security allows such a insignificant group operate on their own doorstep is beyond me.\r\n      Get a patrol down there and clean them out."
+    },
+    {
+      "id": 8,
+      "title": "Kidnapping at County Crossing",
+      "story": "Reported child kidnapped by Gunners. Likely for ransom, further investigation warranted.\r\n      Recommend dispatching a patrol to the area to assess the situation and offer assistance."
+    },
+    {
+      "id": 9,
+      "title": "Stolen Sweetroll?",
+      "story": "Mama Murphey reports someone stole her Sweetroll. Patrol already in the area investigated, found Mama Murphey to be under the influence of Grape Mentats and Jet. False alarm."
+    }
+  ],
+  "picturesDcr": [
+    {
+      "id": "1",
+      "name": "corvega.jpg"
+    },
+    {
+      "id": "2",
+      "name": "arcjet.jpg"
+    },
+    {
+      "id": "3",
+      "name": "Soul_survivor.jpg"
+    },
+    {
+      "id": "4",
+      "name": "university_point.jpg"
+    },
+    {
+      "id": "5",
+      "name": "piper.jpg"
+    }
+  ],
+  "picturesMinutemen": [
+    {
+      "id": "1",
+      "name": "slog.jpg"
+    },
+    {
+      "id": "2",
+      "name": "abernathy.jpg"
+    },
+    {
+      "id": "3",
+      "name": "red_rocket.jpg"
+    },
+    {
+      "id": "4",
+      "name": "greentop.jpg"
+    },
+    {
+      "id": "5",
+      "name": "mr_gutsy.jpg"
+    }
+  ],
+}
+
     // static headlines and pictures
     document.getElementById('main-blurb').addEventListener('click', learnMore)
     document.getElementById('main-blurb').innerHTML = arr.dcr[0].title
@@ -135,28 +282,28 @@ function nonSynth(e) {
 }
 
 // post function
-let newReport = {
-  report: `A Deathclaw ate my homework!`
-}
+// let newReport = {
+//   report: `A Deathclaw ate my homework!`
+// }
 // submission button
 document.getElementById('submit-button').addEventListener('click', post)
 
 function post(e) {
   e.preventDefault()
-  fetch(`http://localhost:3000/reports`, {
-      method: 'post',
-      body: JSON.stringify(newReport),
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    })
-    .then(response => {
-      console.log('the server response', response)
-      return response.json()
-    })
-    .then(data => {
+  // fetch(`http://localhost:3000/reports`, {
+  //     method: 'post',
+  //     body: JSON.stringify(newReport),
+  //     headers: new Headers({
+  //       'Content-Type': 'application/json'
+  //     })
+  //   })
+  //   .then(response => {
+  //     console.log('the server response', response)
+  //     return response.json()
+  //   })
+  //   .then(data => {
       window.alert('Your report will be reviewed by Ronnie Shaw in the order it was recieved');
-      console.log('data', data)
-    })
+  //     console.log('data', data)
+  //   })
 }
-})
+// })
